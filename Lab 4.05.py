9f6b5fec8c0a5b33b5276b7a4716cd6ad5f04182
Evander <ehrich22@students.bmchs.org>
2022-04-25 09:27:18 -0500

    debug

---
+++ Lab 4.05.py
@@ -0,0 +1,63 @@
+'''
+##########
+Lab 4.05
+##########
+
+1. Read through the following code
+    def print_numbers(list):
+        for i in range(1, len(list)+1):
+            print(list[i])
+​
+    num_list = [1, 2, 3, 4, 5, 6]
+    print_numbers(num_list)
+In your notebook
+Write down any bugs that you see in the program
+
+1- Index is out of range: change 1 to 0
+2- Do not need to call for an integer; remove [i]
+
+2. Read through the following code
+    def swapping_stars():
+    line_str = ""
+        for line in range(0, 6):
+            for char in range(0,6):
+                if line % 2 == char % 2:
+                    line_str += "*"
+                else:
+                    line_str += "-"
+    print(line_str)
+Continue in your notebook
+Write down any bugs that you see in the program
+
+1-Incorrect indent with second and third line
+
+
+3. In script mode
+Fix the 2 programs above.
+Create a program that asks the user which function they would like to run.
+'''
+
+def print_numbers(list):
+    for i in range(0, len(list)+1):
+        print(list)
+
+
+
+
+def swapping_stars():
+    line_str = ""
+    for line in range(0, 6):
+        for char in range(0,6):
+            if line % 2 == char % 2:
+                line_str += "*"
+            else:
+                line_str += "-"
+    print(line_str)
+
+while True:
+    user_choice = input("One or two?")
+    if user_choice == '1':
+        num_list = [1, 2, 3, 4, 5, 6]
+        print_numbers(num_list)
+    elif user_choice == '2':
+        swapping_stars()
